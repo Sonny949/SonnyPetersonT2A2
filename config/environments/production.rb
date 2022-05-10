337cd4bcd5c9c43fe3f34b2e858697b1638aee67
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-10 17:40:14 +1000

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,14 +68,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'https://whispering-refuge-44028.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for mailcatcher gem.
-  config.action_mailer.smtp_settings = {
-    address: '127.0.0.1',
-    port: 1025
-  }
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test 
+  config.action_mailer.default_url_options = { host: localhost:3000 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
