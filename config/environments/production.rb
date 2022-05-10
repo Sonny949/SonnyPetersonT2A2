e38fb75ae4b3189e66d582d543788b88bf1bf8d2
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-10 17:36:33 +1000

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,13 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'https://whispering-refuge-44028.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailcatcher gem.
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
+  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
