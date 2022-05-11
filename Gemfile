0dd3691b607ebf5a3689f512063fcf31dabb46e7
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-11 15:19:49 +1000

    added rolify and pundit

--- Gemfile
+++ Gemfile
@@ -77,3 +77,7 @@ end
 gem "devise", "~> 4.8"
 
 gem "nested_form", "~> 0.3.2"
+
+gem "rolify", "~> 6.0"
+
+gem "pundit", "~> 2.2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,13 @@ Category.destroy_all
 Listing.destroy_all
 
 user = User.create(email: 'sonny@test.com', password: 'password', name: 'Sonny Peterson', phone_number: '2222222222')
+user.add_role :admin
+
+editor = User.create(email: 'editor@editorial.com', password: 'password', name: 'Max Power', phone_number: '4444444444')
+editor.add_role :editor
 
 user_address = Address.create(user_id: user.id, address_first: '123 Scoop St', address_second: 'Walkervale', city: 'Bundaberg', state: 'QLD', postcode: '4670')
+editor_address = Address.create(user_id: editor.id, address_first: '116 Scoop St', address_second: 'Walkervale', city: 'Bundaberg', state: 'QLD', postcode: '4670')
 
 boats = Category.create(name: 'Boats')
 hulls = Category.create(name: 'Hulls')
