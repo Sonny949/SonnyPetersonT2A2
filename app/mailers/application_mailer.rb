87605f5c19455f6d3f237676c5cd3fe97e78f9ef
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-13 22:50:34 +1000

    trying again

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
-
-  after_action do
-    self.delivery_method = SETTING_FROM_DB.delivery_method
-    settings = {
-      address: SETTING_FROM_DB.address,
-      port: SETTING_FROM_DB.port
-      # More setting if needed
-      }
-    self.mail.delivery_method.settings.merge! settings
-   end
 end
