fbdd3774c93476bc2ae72e488d7ed58a76e66c79
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-13 22:25:53 +1000

    trying again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,10 +43,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for mailcatcher gem.
-  config.action_mailer.smtp_settings = {
-    address: '127.0.0.1',
-    port: 1025
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: '127.0.0.1',
+  #   port: 1025
+  # }
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
