64713ce1f4a7fea5dbcf9b46d3466e524c51032d
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-13 14:44:35 +1000

    .env complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,19 +16,29 @@ before_action :set_posts, only: [:show, :update, :destroy, :edit]
 
   def create
     @post = current_user.posts.create(post_params)
-    redirect_to posts_path
+    if @post.valid?
+      redirect_to @post
+    else
+      flash.now[:alert] = @post.errors.full_messages.join('<br>')
+      # so any entered form data stays on page
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    begin
     @post.update!(post_params)
     redirect_to @post
+    rescue
+      flash.now[:alert] = @post.errors.full_messages.join('<br>')
+      render 'edit'
+    end
   end
 
   def destroy
-    # @post.image.purge
     @post.destroy
     redirect_to posts_path
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   # there can be one image attached for a post
   has_one_attached :image # has_many_attached 
+
+  validates :title, presence: :true
+  validates :description, presence: :true, length: { minimum: 100 }
 end
