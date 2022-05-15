702fcd7e826abb9b4b9c88b8474f96a363dfdb13
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-15 18:18:51 +1000

    added dependent destroy to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   rolify
   has_one :address, dependent: :destroy
-  has_many :listings
-  has_many :posts
+  has_many :listings, dependent: :destroy
+  has_many :posts, dependent: :destroy
   has_many :buying_user_shipments, class_name: 'Shipment', foreign_key: 'buying_user_id'
   has_many :selling_user_shipments, class_name: 'Shipment', foreign_key: 'selling_user_id'
   accepts_nested_attributes_for :address
