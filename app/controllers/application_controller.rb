45727b320846fd0db1204c0664f355d428a9f441
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-17 18:10:09 +1000

    found and fixed error in devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, address_attributes: [:address_first, :address_second, :state, :city, :postal_code]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, address_attributes: [:address_first, :address_second, :state, :city, :postcode]])
   end
 
   private
