542991e83daeaf48b5e2798a8fb997f8a7e66ae5
Sonny949 <sonnypeterson949@hotmail.com>
2022-05-18 21:25:05 +1000

    testing live

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,5 @@
 class PaymentsController < ApplicationController
-# skip_before_action :verify_authenticity_token, only: [:webhook]
+skip_before_action :verify_authenticity_token, only: [:webhook]
 
   def success
   end
